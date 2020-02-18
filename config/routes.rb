fa622bdfd6f0a18488948fde64005e3cbdc3c148
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-02-18 21:33:34 +0800

    tes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   resources :roles
   resources :permissions
   devise_for :users
-  resources :b_twelves
-  resources :b_eights
-  resources :b_fours
-  resources :action_plans
+
+  resources :action_plans do
+    resources :b_fours
+    resources :b_eights
+    resources :b_twelves
+  end
+  
   resources :cities
   resources :years
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
