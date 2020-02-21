13284b556463edd95d6769941a1fb49874588593
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-02-21 15:16:52 +0800

    tes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+permissions =[
+    {
+        name: 'Manajemen Tahun', #Penamaan gasan permission nya
+        resource: 'year', #nama file di model harus sama
+        action: 'index', #nama function di controller
+        description: 'Manajemen data tahun', #gasan penjelasan aja
+        page: true, #ini akan bisa di akses di menu atau tidak
+        path: 'years_path' #path di rails route
+    },
+    {
+        name: 'Create Tahun', #Penamaan gasan permission nya
+        resource: 'year', #nama file di model harus sama
+        action: 'new', #nama function di controller
+        description: 'Tambah data tahun', #gasan penjelasan aja
+        page: false, #ini akan bisa di akses di menu atau tidak
+        path: 'new_year_path' #path di rails route
+    },
+]
