b864c59d197f9f263a6e733fb08e16680d74fb4b
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-02-21 15:04:27 +0800

    setup auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+    include Pundit
+    protect_from_forgery
+
     attr_writer :login
 
     def login
