b3886ca676ee71a248c62d16a1ebdd3f996d67a5
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-02-23 03:48:13 +0800

    sikuttt

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,16 @@
 class DashboardController < ApplicationController
   def user
     @years = Year.all
+
+    if policy(:dashboard).user?
+    else
+      redirect_to dashboard_admin_path
+    end
   end
 
   def admin
     @cities = City.all
+
     if policy(:dashboard).admin?
     else
       redirect_to dashboard_user_path
