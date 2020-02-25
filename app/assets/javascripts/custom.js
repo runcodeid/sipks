a9379732ab9e08bc1549b3a4d2bc08b6eb2067eb
Nat4Lia <raztajr@gmail.com>
2020-02-26 01:46:32 +0800

    remove link show years, add invalid login notification, remove dashboard & alternative link, remove method delete on logout

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -32,6 +32,40 @@ $('div.notifikasi').show(function(){
     $(this).hide();
 });
 
+$('div.sessions-notification').show(function(){
+  notifMessage = $(this).data('notifikasi');
+  $.notify({
+      icon: "ni ni-bell-55",
+      title: notifMessage,
+      message: "",
+      url: ""
+  }, {
+      element: "body",
+      type: "danger",
+      allow_dismiss: true,
+      placement: {
+          from: "top",
+          align: "center"
+      },
+      offset: {
+          x: 15,
+          y: 15
+      },
+      spacing: 10,
+      z_index: 1080,
+      delay: 3000,
+      timer: 1000,
+      url_target: "_blank",
+      mouse_over: !1,
+      animate: {
+          enter: "animated fadeInDown",
+          exit: "animated fadeOutUp"
+      },
+      template: '<div data-notify="container" class="alert alert-dismissible alert-{0} alert-notify" role="alert"><span class="alert-icon" data-notify="icon"></span> <div class="alert-text"</div> <span class="alert-title" data-notify="title">{1}</span> <span data-notify="message">{2}</span></div><button type="button" class="close" data-notify="dismiss" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
+  });
+  $(this).hide();
+});
+
 //Custom Input File
 var inputs = document.querySelectorAll( '.custom-file-input' );
 Array.prototype.forEach.call( inputs, function( input )
