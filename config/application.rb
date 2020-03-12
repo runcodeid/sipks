ad4918fe8925935f65b14b3a6753f3a4a2fdc572
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-03-12 23:33:54 +0800

    add setup assets

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Sipks
 
     config.action_dispatch.rescue_responses["Pundit::NotAuthorizedError"] = :forbidden
 
+    config.serve_static_assets = true
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
